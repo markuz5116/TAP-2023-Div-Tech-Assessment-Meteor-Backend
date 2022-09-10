9d23d9ef44be9f68c70f49d599e8284000bc841e
Marcus Ong <markuz5116@gmail.com>
2022-09-10 17:58:28 +0800

    Create household

---
+++ household/model/household.py
@@ -0,0 +1,4 @@
+class Household():
+    def __init__(self, household_type, household_members) -> None:
+        self.household_type = household_type
+        self.household_members = household_members
