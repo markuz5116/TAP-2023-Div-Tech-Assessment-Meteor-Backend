edf9459f8fc3a21d1486e0e4ccc714a44560380e
Marcus Ong <markuz5116@gmail.com>
2022-09-10 19:58:41 +0800

    Add insert household functionality

--- household/index.py
+++ household/index.py
@@ -1,7 +1,6 @@
-import json
 import os
 import psycopg2
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,12 +11,21 @@ def connect_to_db():
         password=os.environ['DB_PASSWORD'])
     return conn
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/household', methods=['POST'])
+def create_household():
+    housing_type = request.args.get('housing_type')
+    if not housing_type:
+       return jsonify({ "Error": 'Missing housing_type'}), 400
+
+    housing_type = housing_type.lower()
+    if housing_type not in ['landed', 'condominium', 'hdb']:
+        return jsonify({ "Error": f'housing_type must be landed, condominium or hdb, got: {housing_type}'}), 403
     conn = connect_to_db()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM people;')
-    people = cur.fetchall()
+    cur.execute(f'SELECT * FROM add_household(\'{housing_type}\')')
+    housing_id = cur.fetchone()[0]
+
+    conn.commit()
     cur.close()
     conn.close()
-    return jsonify(people)
+    return jsonify({"Success": f'House added with id: {housing_id}'}), 201
--- init_db.py
+++ init_db.py
@@ -11,12 +11,15 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS households;')
-cur.execute('DROP TABLE IF EXISTS people;')
+cur.execute('DROP FUNCTION IF EXISTS add_household;')
 
-cur.execute('CREATE TABLE households(hid varchar(50) PRIMARY KEY,'
-    'householdType varchar(50) NOT NULL,'
-    'check (householdType in (\'Landed\', \'Condominium\', \'HDB\'))'
+cur.execute('DROP TABLE IF EXISTS eligible_schemes_for_people;')
+cur.execute('DROP TABLE IF EXISTS people;')
+cur.execute('DROP TABLE IF EXISTS households;')
+
+cur.execute('CREATE TABLE households(hid INTEGER PRIMARY KEY,'
+    'housingType varchar(20) NOT NULL,'
+    'check (housingType in (\'landed\', \'condominium\', \'hdb\'))'
     ');'
     )
 
@@ -28,7 +31,7 @@ cur.execute('CREATE TABLE people(pid varchar(9) PRIMARY KEY,'
     'occupationType varchar(50) NOT NULL,'
     'annualIncome numeric(10, 2),'
     'dob date NOT NULL,'
-    'hid varchar(50) REFERENCES households(hid)'
+    'hid INTEGER REFERENCES households(hid)'
     'check (annualIncome >= 0),'
     'check (gender in (\'male\', \'female\', \'prefer not to say\'))'
     ');'
@@ -37,21 +40,41 @@ cur.execute('CREATE TABLE people(pid varchar(9) PRIMARY KEY,'
 cur.execute('CREATE TABLE eligible_schemes_for_people(scheme_name VARCHAR(100) NOT NULL,'
     'pid varchar(9) NOT NULL REFERENCES people(pid),'
     'PRIMARY KEY (scheme_name, pid)'
+    ');'
     )
 
 # Add sample inputs
 cur.execute('INSERT INTO households values'
-    '(\'h1\', \'Landed\'),'
-    '(\'h2\', \'Condominium\'),'
-    '(\'h3\', \'HDB\');'
+    '(1, \'landed\'),'
+    '(2, \'condominium\'),'
+    '(3, \'hdb\');'
     )
 
 cur.execute('INSERT INTO people values'
-    '(\'S1111111A\', \'person1\', \'male\', \'single\', NULL, \'studying\', 1, \'1998-12-19\', \'h1\'),'
-    '(\'T2222222B\', \'person2\', \'female\', \'married\', \'S3333333C\', \'full-time\', 2000, \'2000-01-01\', \'h2\'),'
-    '(\'S3333333C\', \'person3\', \'male\', \'attached\', \'T2222222B\', \'part-time\', 300, \'1990-06-30\', \'h2\'),'
-    '(\'T4444444B\', \'person4\', \'prefer not to say\', \'single\', NULL, \'studying\', NULL, \'1998-12-19\', \'h3\');'
+    '(\'S1111111A\', \'person1\', \'male\', \'single\', NULL, \'studying\', 1, \'1998-12-19\', 1),'
+    '(\'T2222222B\', \'person2\', \'female\', \'married\', \'S3333333C\', \'full-time\', 2000, \'2000-01-01\', 2),'
+    '(\'S3333333C\', \'person3\', \'male\', \'attached\', \'T2222222B\', \'part-time\', 300, \'1990-06-30\', 2),'
+    '(\'T4444444B\', \'person4\', \'prefer not to say\', \'single\', NULL, \'studying\', NULL, \'1998-12-19\', 3);'
     )
+
+cur.execute('''
+    CREATE OR REPLACE FUNCTION add_household(IN housingType VARCHAR(20))
+    RETURNS INT AS $$
+    DECLARE 
+        maxId INTEGER := 0;
+    BEGIN
+        SELECT max(hid) INTO maxId
+        FROM households;
+
+        maxId := maxId + 1;
+
+        INSERT INTO households values
+        (maxId, housingType);
+
+        RETURN maxId;
+    END;
+    $$ LANGUAGE plpgsql;
+''')
 
 
 conn.commit()
