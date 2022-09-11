b41eef3a42a9e5da38abc8b321ee5e500d9bf5b1
Marcus Ong <markuz5116@gmail.com>
2022-09-11 17:35:30 +0800

    Add get_eligible_members for grant in db

--- init_db.py
+++ init_db.py
@@ -15,6 +15,7 @@ cur.execute('DROP FUNCTION IF EXISTS add_household;')
 cur.execute('DROP FUNCTION IF EXISTS add_family_member;')
 cur.execute('DROP FUNCTION IF EXISTS list_households;')
 cur.execute('DROP FUNCTION IF EXISTS get_household;')
+cur.execute('DROP FUNCTION IF EXISTS get_eligible_members;')
 
 cur.execute('DROP TABLE IF EXISTS eligible_schemes_for_people;')
 cur.execute('DROP TABLE IF EXISTS people;')
@@ -103,6 +104,18 @@ cur.execute('''
             SELECT H.hid, H.housingType, P.name, P.gender, P.maritalStatus, P.spouse, P.occupationType, P.annualIncome, P.dob
             FROM people P natural join households H
             WHERE H.hid = inHid;
+    END;
+    $$ LANGUAGE plpgsql;
+''')
+
+cur.execute('''
+    CREATE OR REPLACE FUNCTION get_eligible_members(IN inSchemeName VARCHAR(100))
+    RETURNS TABLE (outHid INTEGER, outHousingType VARCHAR(20), outName VARCHAR(50), outGender VARCHAR(20), outMaritalStatus VARCHAR(50), outSpouse VARCHAR(9), outOccupationType VARCHAR(50), outAnnualIncome NUMERIC(10, 2), outDob DATE) AS $$
+    BEGIN
+        RETURN QUERY
+            SELECT H.hid, H.housingType, P.name, P.gender, P.maritalStatus, P.spouse, P.occupationType, P.annualIncome, P.dob
+            FROM eligible_schemes_for_people E natural join people P natural join households H
+            WHERE E.schemeName = inSchemeName; 
     END;
     $$ LANGUAGE plpgsql;
 ''')
